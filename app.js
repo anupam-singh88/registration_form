14536eee9efdc74dfbba4fed67a88dd1f77a9245
anupam singh <anupamsingh2389@gmail.com>
2023-09-28 10:00:13 +0530

    added email validation , view password option , regex for password to contain atleast one uppercase, lowercase and 8 characters

--- app.js
+++ app.js
@@ -39,18 +39,6 @@ function checkVal() {
 
     }
 
-    if (uEmail.value.length >= 5) {
-        uEmail.nextElementSibling.nextElementSibling.classList.add("chkVisible")
-        uEmail.nextElementSibling.classList.remove("wrongVis")
-
-    }
-    if (uEmail.value.length <= 5) {
-        uEmail.nextElementSibling.nextElementSibling.classList.remove("chkVisible")
-        // uEmail.nextElementSibling.classList.add("wrongVis")
-        // return false
-
-    }
-
     if (phNo.value.length >= 5) {
         phNo.nextElementSibling.nextElementSibling.classList.add("chkVisible")
         phNo.nextElementSibling.classList.remove("wrongVis")
@@ -64,17 +52,17 @@ function checkVal() {
     }
 
 
-    if (password.value.length >= 3) {
-        password.nextElementSibling.nextElementSibling.classList.add("chkVisible")
-        password.nextElementSibling.classList.remove("wrongVis")
+    // if (password.value.length >= 3) {
+    //     password.nextElementSibling.nextElementSibling.classList.add("chkVisible")
+    //     password.nextElementSibling.classList.remove("wrongVis")
 
-    }
-    if (password.value.length <= 3) {
-        password.nextElementSibling.nextElementSibling.classList.remove("chkVisible")
-        // password.nextElementSibling.classList.add("wrongVis")
-        // return false
+    // }
+    // if (password.value.length <= 3) {
+    //     password.nextElementSibling.nextElementSibling.classList.remove("chkVisible")
+    //     // password.nextElementSibling.classList.add("wrongVis")
+    //     // return false
 
-    }
+    // }
 }
 function comparePassword() {
     if (cPassword.value === password.value) {
@@ -89,76 +77,19 @@ function comparePassword() {
     }
 }
 
-// function submitHandler(e) {
-//     e.preventDefault()
-// }
 submitBtn.addEventListener('click', (e) => {
     e.preventDefault()
-    if (fName.value === '') {
-        fName.nextElementSibling.classList.add("wrongVis")
-        alert('Kindly Enter First Name')
-
-        return false;
-    }
-    if (uName.value === '') {
-        uName.nextElementSibling.classList.add("wrongVis")
-        alert('Kindly Enter User Name')
-
-        return false;
-    }
-    if (uEmail.value === '') {
-        uEmail.nextElementSibling.classList.add("wrongVis")
-        alert('Kindly Enter Email')
-
-        return false;
-    }
-    if (phNo.value === '') {
-        phNo.nextElementSibling.classList.add("wrongVis")
-        alert('Kindly Enter Phone Number')
-
-        return false;
-    }
-    if (password.value === '') {
-        password.nextElementSibling.classList.add("wrongVis")
-        alert('Kindly Enter Password')
-
-        return false;
-    }
-    if (cPassword.value === '') {
-        cPassword.nextElementSibling.classList.add("wrongVis")
-        alert('Kindly enter the confirm password')
-
-        return false;
-    }
-    if (cPassword.value !== password.value) {
-        cPassword.nextElementSibling.classList.add("wrongVis")
-        alert('Password Not Matched!!')
-
-        return false;
-    }
-    // if (fName.value === '' || uName.value === '' || uEmail === '' || phNo.value === '' || password.value === '' || cPassword.value === '') {
-    //     alert('fill all details')
-    //     return false;
-    // }
-    if (genderMale.checked) {
-        // console.log('male')
-        alert('submitted')
-        clearValues()
+    checkValues();
+    if (validateEmail() && checkValues() && validatePass()) {
+        alert('data submitted')
+        clearValues();
         return true
-    } else if (genderfeMale.checked) {
-        alert('submitted')
-        clearValues()
-
-        return true
-    } else if (genderNone.checked) {
-        alert('submitted')
-        clearValues()
-
-        return true
-    } else {
-        alert('select gender')
-        return false
     }
+    else {
+        alert('Please fill all the fields correctly!!! Password must contain atleast one special ,one uppercase,one letter and one numeric value');
+    }
+
+    // alert('Please fill all the fields correctly!!! Password must contain atleast one special ,one uppercase,one letter and one numeric value');
 
 
 })
@@ -178,4 +109,108 @@ function clearValues() {
     genderMale.checked = false;
     genderfeMale.checked = false;
     genderNone.checked = false;
+}
+
+function validateEmail() {
+    let regex = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/;
+    let result = regex.test(uEmail.value);
+    if (result) {
+        uEmail.nextElementSibling.nextElementSibling.classList.add("chkVisible")
+        uEmail.nextElementSibling.classList.remove("wrongVis")
+        return true
+
+    } else {
+        uEmail.nextElementSibling.nextElementSibling.classList.remove("chkVisible")
+        uEmail.nextElementSibling.classList.add("wrongVis")
+        return false;
+    }
+
+}
+function validatePass() {
+    let regex = /^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}$/;
+    let result = regex.test(password.value);
+    if (result) {
+        password.nextElementSibling.nextElementSibling.classList.add("chkVisible")
+        password.nextElementSibling.classList.remove("wrongVis")
+        // console.log('password correct')
+        return true
+
+    } else {
+        password.nextElementSibling.nextElementSibling.classList.remove("chkVisible")
+        password.nextElementSibling.classList.add("wrongVis")
+        // console.log('password incorrect')
+        return false;
+    }
+
+}
+
+function checkValues() {
+    if (fName.value === '') {
+        fName.nextElementSibling.classList.add("wrongVis")
+        // alert('Kindly Enter First Name')
+
+        return false;
+    }
+    if (uName.value === '') {
+        uName.nextElementSibling.classList.add("wrongVis")
+        // alert('Kindly Enter User Name')
+
+        return false;
+    }
+    if (uEmail.value === '') {
+        uEmail.nextElementSibling.classList.add("wrongVis")
+        // alert('Kindly Enter Email')
+
+        return false;
+    }
+    if (phNo.value === '') {
+        phNo.nextElementSibling.classList.add("wrongVis")
+        // alert('Kindly Enter Phone Number')
+
+        return false;
+    }
+    if (password.value === '') {
+        password.nextElementSibling.classList.add("wrongVis")
+        // alert('Kindly Enter Password')
+
+        return false;
+    }
+    if (cPassword.value === '') {
+        cPassword.nextElementSibling.classList.add("wrongVis")
+        // alert('Kindly enter the confirm password')
+
+        return false;
+    }
+    if (cPassword.value !== password.value) {
+        cPassword.nextElementSibling.classList.add("wrongVis")
+        // alert('Password Not Matched!!')
+
+        return false;
+    }
+
+    if (genderMale.checked || genderfeMale.checked || genderNone.checked) {
+        return true
+    } else {
+        // alert('select gender')
+        return false
+    }
+
+}
+
+
+
+function password_show_hide() {
+    var x = document.getElementById("password");
+    var show_eye = document.getElementById("show_eye");
+    var hide_eye = document.getElementById("hide_eye");
+    hide_eye.classList.remove("d-none");
+    if (x.type === "password") {
+        x.type = "text";
+        show_eye.style.display = "none";
+        hide_eye.style.display = "block";
+    } else {
+        x.type = "password";
+        show_eye.style.display = "block";
+        hide_eye.style.display = "none";
+    }
 }
